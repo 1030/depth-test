8a6eb796b16411c81f0f8771c6d3ca230d7bb0cf
1030 <jim@1030.co.uk>
2023-03-19 22:52:30 +0000

    Update depth_test.js

--- depth_test.js
+++ depth_test.js
@@ -1,31 +1,42 @@
-const canvas = document.getElementById('myCanvas');
-const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const objectWidth = 50;
-const objectHeight = 50;
-const minDepth = 0.5;
-const maxDepth = 2;
+let scene, camera, renderer;
+let geometry, material, mesh;
+let clones = 5;
 let depth = 1;
 let xShift = 0;
 
-// Calculate the number of clones needed to fill the horizontal space
-const clones = Math.ceil(canvas.width / objectWidth);
+init();
+animate();
 
-function drawObjects() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'blue';
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    const baseX = 0;
-    const baseY = (canvas.height - objectHeight) / 2 + xShift;
-    const w = objectWidth * depth;
-    const h = objectHeight * depth;
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    geometry = new THREE.BoxGeometry(1, 1, 1);
+    material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
     for (let i = 0; i < clones; i++) {
-        const x = baseX + i * objectWidth;
-        ctx.fillRect(x, baseY, w, h);
+        mesh = new THREE.Mesh(geometry, material);
+        mesh.position.x = i * 2;
+        scene.add(mesh);
     }
+
+    camera.position.z = 5;
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    for (let i = 0; i < clones; i++) {
+        const obj = scene.children[i];
+        obj.position.x = i * 2 + xShift;
+        obj.scale.set(depth, depth, depth);
+    }
+
+    renderer.render(scene, camera);
 }
 
 function requestAccess() {
@@ -48,7 +59,13 @@ function requestAccess() {
 function handleOrientation(event) {
     const { alpha, beta, gamma } = event;
 
-    // Map beta (-90 to 90) to depth (minDepth to maxDepth)
-    depth = mapRange(beta, -90, 90, minDepth, maxDepth);
+    // Map beta (-90 to 90) to depth (0.5 to 2)
+    depth = mapRange(beta, -90, 90, 0.5, 2);
 
-    // Map gamma (-180 to 180) to xShift (-canvas.height / 2 to canvas.height / 
+    // Map gamma (-180 to 180) to xShift (-2 to 2)
+    xShift = mapRange(gamma, -180, 180, -2, 2);
+}
+
+function mapRange(value, inMin, inMax, outMin, outMax) {
+    return ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+}
