7c846e1675b8c74efa822f85a2c91251d06819b6
1030 <jim@1030.co.uk>
2023-03-19 22:59:14 +0000

    Update depth_test.js

--- depth_test.js
+++ depth_test.js
@@ -8,64 +8,41 @@ init();
 animate();
 
 function init() {
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
-    geometry = new THREE.BoxGeometry(1, 1, 1);
-    material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  geometry = new THREE.BoxGeometry(1, 1, 1);
+  material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
-    for (let i = 0; i < clones; i++) {
-        mesh = new THREE.Mesh(geometry, material);
-        mesh.position.x = i * 2;
-        scene.add(mesh);
-    }
+  for (let i = 0; i < clones; i++) {
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.x = i * 2;
+    scene.add(mesh);
+  }
 
-    camera.position.z = 5;
+  camera.position.z = 5;
 }
 
 function animate() {
-    requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
 
-    for (let i = 0; i < clones; i++) {
-        const obj = scene.children[i];
-        obj.position.x = i * 2 + xShift;
-        obj.scale.set(depth, depth, depth);
-    }
+  for (let i = 0; i < clones; i++) {
+    const obj = scene.children[i];
+    obj.position.x = i * 2 + xShift;
+    obj.scale.set(depth, depth, depth);
+  }
 
-    renderer.render(scene, camera);
+  renderer.render(scene, camera);
 }
 
-function requestAccess() {
-    if (typeof DeviceMotionEvent.requestPermission === 'function') {
-        DeviceMotionEvent.requestPermission()
-            .then(permissionState => {
-                if (permissionState === 'granted') {
-                    window.addEventListener('deviceorientation', handleOrientation);
-                } else {
-                    alert('Permission not granted. Motion events will not be accessible.');
-                }
-            })
-            .catch(console.error);
-    } else {
-        // No need for permission on non-iOS devices
-        window.addEventListener('deviceorientation', handleOrientation);
-    }
+function handleDepthSliderChange(value) {
+  depth = value;
 }
 
-function handleOrientation(event) {
-    const { alpha, beta, gamma } = event;
-
-    // Map beta (-90 to 90) to depth (0.5 to 2)
-    depth = mapRange(beta, -90, 90, 0.5, 2);
-
-    // Map gamma (-180 to 180) to xShift (-2 to 2)
-    xShift = mapRange(gamma, -180, 180, -2, 2);
-}
-
-function mapRange(value, inMin, inMax, outMin, outMax) {
-    return ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+function handlePositionSliderChange(value) {
+  xShift = value;
 }
