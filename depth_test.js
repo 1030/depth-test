ea4f688277995f303962722a86fbdb70e4d65d85
1030 <jim@1030.co.uk>
2023-03-19 22:49:28 +0000

    Update depth_test.js

--- depth_test.js
+++ depth_test.js
@@ -8,60 +8,47 @@ const objectHeight = 50;
 const minDepth = 0.5;
 const maxDepth = 2;
 let depth = 1;
-let yShift = 0;
+let xShift = 0;
 
 // Calculate the number of clones needed to fill the horizontal space
-const clones = Math.ceil(canvas.height / objectHeight);
+const clones = Math.ceil(canvas.width / objectWidth);
 
 function drawObjects() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'blue';
 
-    const baseX = (canvas.width - objectWidth) / 2 + yShift;
-    const baseY = 0;
+    const baseX = 0;
+    const baseY = (canvas.height - objectHeight) / 2 + xShift;
     const w = objectWidth * depth;
     const h = objectHeight * depth;
 
     for (let i = 0; i < clones; i++) {
-        const y = baseY + i * objectHeight;
-        ctx.fillRect(baseX, y, w, h);
+        const x = baseX + i * objectWidth;
+        ctx.fillRect(x, baseY, w, h);
     }
 }
 
-// Rest of the code remains the same
-
-
-
-function drawReferenceDots() {
-  const dotSize = 5;
-  const dotSpacing = 50;
-
-  ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
-
-  for (let y = dotSpacing; y < canvas.height; y += dotSpacing) {
-    for (let x = dotSpacing; x < canvas.width; x += dotSpacing) {
-      ctx.beginPath();
-      ctx.arc(x, y, dotSize, 0, 2 * Math.PI);
-      ctx.fill();
+function requestAccess() {
+    if (typeof DeviceMotionEvent.requestPermission === 'function') {
+        DeviceMotionEvent.requestPermission()
+            .then(permissionState => {
+                if (permissionState === 'granted') {
+                    window.addEventListener('deviceorientation', handleOrientation);
+                } else {
+                    alert('Permission not granted. Motion events will not be accessible.');
+                }
+            })
+            .catch(console.error);
+    } else {
+        // No need for permission on non-iOS devices
+        window.addEventListener('deviceorientation', handleOrientation);
     }
-  }
 }
 
-function handleTilt(event) {
-  const beta = event.beta;
-  const gamma = event.gamma;
+function handleOrientation(event) {
+    const { alpha, beta, gamma } = event;
 
-  // Map the tilt angle (beta) to the depth range
-  depth = ((beta + 90) % 180 - 90) / 180 * (maxDepth - minDepth) + minDepth;
+    // Map beta (-90 to 90) to depth (minDepth to maxDepth)
+    depth = mapRange(beta, -90, 90, minDepth, maxDepth);
 
-  // Clamp depth value between minDepth and maxDepth
-  depth = Math.max(minDepth, Math.min(maxDepth, depth));
-
-  // Calculate yShift based on the gamma angle
-  yShift = -gamma * (canvas.height / 180);
-
-  drawReferenceDots();
-  drawObjects();
-}
-
-// Rest of the code remains the same
+    // Map gamma (-180 to 180) to xShift (-canvas.height / 2 to canvas.height / 
